f0e722ca9a67a5045e41e83c22941a77513063a3
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-09-30 16:45:32 -0300

    M  README.md M  src/Upload.php

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # upload
 :arrow_up: Sistema básico de upload
+
+## como usar
+```
+$Upload=new Upload();
+$exts=[
+'jpg',
+'png',
+'gif'
+];
+$file=$Upload->upload($name,$exts);
+if($file){
+    print 'arquivo enviado com sucesso';
+}else{
+    print '<pre>';
+    var_dump($file);
+}
+```
+
+## mensagens de erro
+```
+invalid_extension
+invalid_size
+unknown_error
+```
--- src/Upload.php
+++ src/Upload.php
@@ -2,5 +2,70 @@
 namespace Basic;
 
 class Upload{
-
+    function upload($name,$exts){
+        $maxSize=max_upload_size();
+        $error=false;
+        switch(@$_FILES[$name]["error"]){
+            case 0:
+            //0. upload bem sucedido
+            $file['size']=@$_FILES[$name]['size'];
+            if($file['size']<max_upload_size()){
+                $file['name']=@$_FILES[$name]['name'];
+                $file['ext']=@pathinfo($_FILES[$name]['name'],PATHINFO_EXTENSION);
+                $file['ext']=strtolower($file['ext']);
+                $file['temp']=@$_FILES[$name]['tmp_name'];
+                if (in_array($file['ext'], $exts)) {
+                    $isImage = getimagesize($_FILES[$name]["tmp_name"]);
+                    if($isImage){
+                        $file['isImage']=true;
+                    }else{
+                        $file['isImage']=false;
+                    }
+                }else{
+                    $error[]='invalid_extension';
+                }
+            }else{
+                $error[]='invalid_size';
+            }
+            break;
+            case 1:
+            case 2:
+            //1. Envie um arquivo com no máximo upload_max_filesize bytes
+            //2. Envie um arquivo com no máximo MAX_FILE_SIZE bytes
+            $error[]='invalid_size';
+            break;
+            default:
+            //Tente novamente
+            $error[]='unknown_error';
+            break;
+        }
+        $file['error']=$error;
+        return $file;
+    }
+    function convert_php_size_to_bytes($sSize)
+    {
+        if ( is_numeric( $sSize) ) {
+            return $sSize;
+        }
+        $sSuffix = substr($sSize, -1);
+        $iValue = substr($sSize, 0, -1);
+        switch(strtoupper($sSuffix)){
+            case 'P':
+            $iValue *= 1024;
+            case 'T':
+            $iValue *= 1024;
+            case 'G':
+            $iValue *= 1024;
+            case 'M':
+            $iValue *= 1024;
+            case 'K':
+            $iValue *= 1024;
+            break;
+        }
+        return $iValue;
+    }
+    function max_upload_size()
+    {
+        return min(convert_php_size_to_bytes(ini_get('post_max_size')), convert_php_size_to_bytes(ini_get('upload_max_filesize')));
+    }
 }
