b48aca447b9b4b5024d2af220f2ec273fa192f16
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-10-15 22:21:52 -0200

    M  src/Upload.php

--- src/Upload.php
+++ src/Upload.php
@@ -1,97 +1,80 @@
 <?php
-
 namespace Basic;
 
 class Upload
 {
     public function upload($name, $exts)
     {
-        $maxSize = $this->max_upload_size();
-        
+        $maxSize = $this->maxUploadSize();
         $error = false;
-        
         switch (@$_FILES[$name]['error']) {
             case 0:
-                //0. upload bem sucedido
-                $file['size'] = @$_FILES[$name]['size'];
-
-                if ($file['size'] < $maxSize) {
-
-                    $file['name'] = @$_FILES[$name]['name'];
-                    $file['ext'] = @pathinfo($_FILES[$name]['name'], PATHINFO_EXTENSION);
-                    $file['ext'] = strtolower($file['ext']);
-                    $file['temp'] = @$_FILES[$name]['tmp_name'];
-
-                    if (in_array($file['ext'], $exts)) {
-                        $isImage = getimagesize($_FILES[$name]['tmp_name']);
-                        
-                        if ($isImage) {
-                            $file['is_image'] = true;
-                        } else {
-                            $file['is_image'] = false;
-                        }
+            $file['size'] = @$_FILES[$name]['size'];
+            if ($file['size'] < $maxSize) {
+                $file['name'] = @$_FILES[$name]['name'];
+                $file['ext'] = @pathinfo($_FILES[$name]['name'], PATHINFO_EXTENSION);
+                $file['ext'] = strtolower($file['ext']);
+                $file['temp'] = @$_FILES[$name]['tmp_name'];
+                if (in_array($file['ext'], $exts)) {
+                    $isImage = getimagesize($_FILES[$name]['tmp_name']);
+                    if ($isImage) {
+                        $file['is_image'] = true;
                     } else {
-                        $error[] = 'invalid_extension';
+                        $file['is_image'] = false;
                     }
                 } else {
-                    $error[] = 'invalid_size';
+                    $error[] = 'invalid_extension';
                 }
-
-                break;
+            } else {
+                $error[] = 'invalid_size';
+            }
+            break;
             case 1:
             case 2:
-                //1. Envie um arquivo com no máximo upload_max_filesize bytes
-                //2. Envie um arquivo com no máximo MAX_FILE_SIZE bytes
-                $error[] = 'invalid_size';
-                break;
+            $error[] = 'invalid_size';
+            break;
             default:
-                //Tente novamente
-                $error[] = 'unknown_error';
-                break;
+            $error[] = 'unknown_error';
+            break;
         }
-        
         if ($error) {
             $file['error'] = $error;
         }
-
         return $file;
     }
 
-    public function convert_php_size_to_bytes($sSize)
+    public function convertPhpSizeToBytes($sSize)
     {
         if (is_numeric($sSize)) {
             return $sSize;
         }
-        
         $sSuffix = substr($sSize, -1);
         $iValue = substr($sSize, 0, -1);
-        
         switch (strtoupper($sSuffix)) {
             case 'P':
-                $iValue *= 1024;
-                break;
+            $iValue *= 1024;
+            break;
             case 'T':
-                $iValue *= 1024;
-                break;
+            $iValue *= 1024;
+            break;
             case 'G':
-                $iValue *= 1024;
-                break;
+            $iValue *= 1024;
+            break;
             case 'M':
-                $iValue *= 1024;
-                break;
+            $iValue *= 1024;
+            break;
             case 'K':
-                $iValue *= 1024;
-                break;
+            $iValue *= 1024;
+            break;
         }
-
         return $iValue;
     }
 
-    public function max_upload_size()
+    public function maxUploadSize()
     {
         return min(
-            $this->convert_php_size_to_bytes(ini_get('post_max_size')),
-            $this->convert_php_size_to_bytes(ini_get('upload_max_filesize'))
+            $this->convertPhpSizeToBytes(ini_get('post_max_size')),
+            $this->convertPhpSizeToBytes(ini_get('upload_max_filesize'))
         );
     }
 }
